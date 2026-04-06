877f4c68981ea9c6177fcc6ef07f9b08b7884f87
KingImma <07066611a@gmail.com>
2026-04-06 11:52:56 +0100

    fix: subscription plan seeder

--- app/Models/SubscriptionPlan.php
+++ app/Models/SubscriptionPlan.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class SubscriptionPlan extends Model
 {
@@ -32,7 +33,9 @@ class SubscriptionPlan extends Model
     {
         return $this->hasMany(Tenant::class);
     }
-
+    /**
+     * @param Builder<Model> $query
+     */
     public function scopeActive(Builder $query): Builder
     {
         return $query->where("is_active", true);

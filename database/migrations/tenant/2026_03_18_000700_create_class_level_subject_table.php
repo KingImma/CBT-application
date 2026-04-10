afc9f4443a2c9ecd96f5c3dd01cc7ff6a8227dc0
KingImma <07066611a@gmail.com>
2026-04-10 14:06:18 +0100

    fix: tenant mgration

--- database/migrations/tenant/2026_03_18_000700_create_class_level_subject_table.php
+++ database/migrations/tenant/2026_03_18_000700_create_class_level_subject_table.php
@@ -6,26 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('class_level_subject', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->foreignUuid('class_level_id')->constrained('class_levels')->restrictOnDelete();
             $table->foreignUuid('subject_id')->constrained('subjects')->restrictOnDelete();
-            $table->boolean('is_complusory');
+            $table->boolean('is_compulsory');
             $table->timestamps();
             
             $table->unique(['class_level_id', 'subject_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('class_level_subject');
     }

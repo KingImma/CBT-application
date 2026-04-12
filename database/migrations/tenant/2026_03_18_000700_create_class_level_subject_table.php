2af5be134bab287c2eb1e5fdfb67d324633d6beb
KingImma <07066611a@gmail.com>
2026-04-12 15:24:47 +0100

    fix: class subject migration

--- database/migrations/tenant/2026_03_18_000700_create_class_level_subject_table.php
+++ database/migrations/tenant/2026_03_18_000700_create_class_level_subject_table.php
@@ -3,16 +3,17 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
     public function up()
     {
         Schema::create('class_level_subject', function (Blueprint $table) {
-            $table->uuid('id')->primary();
+            $table->uuid('id')->primary()->default(DB::raw('gen_random_uuid()'));
             $table->foreignUuid('class_level_id')->constrained('class_levels')->restrictOnDelete();
             $table->foreignUuid('subject_id')->constrained('subjects')->restrictOnDelete();
-            $table->boolean('is_compulsory');
+            $table->boolean('is_compulsory')->default(false);
             $table->timestamps();
             
             $table->unique(['class_level_id', 'subject_id']);

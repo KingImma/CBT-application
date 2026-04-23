24f242426b70361e451b49e1933dbbd81ccb576d
KingImma <07066611a@gmail.com>
2026-04-23 14:32:17 +0100

    fix; PHP-FPM startup race and email job queue

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 set -e
 
-echo "==> Waiting 5 seconds for Docker internal DNS to initialize..."
-sleep 5
+# echo "==> Waiting 5 seconds for Docker internal DNS to initialize..."
+# sleep 5
 
-echo "==> Manually nuking compiled caches to prevent boot crashes..."
-rm -f bootstrap/cache/config.php
-rm -f bootstrap/cache/events.php
-rm -f bootstrap/cache/packages.php
-rm -f bootstrap/cache/routes.php
-rm -f bootstrap/cache/services.php
+# echo "==> Manually nuking compiled caches to prevent boot crashes..."
+# rm -f bootstrap/cache/config.php
+# rm -f bootstrap/cache/events.php
+# rm -f bootstrap/cache/packages.php
+# rm -f bootstrap/cache/routes.php
+# rm -f bootstrap/cache/services.php
 
-echo "==> Clearing application cache..."
-php artisan cache:clear
+# echo "==> Clearing application cache..."
+# php artisan cache:clear
 
-echo "==> Discovering packages..."
-php artisan package:discover --ansi
+# echo "==> Discovering packages..."
+# php artisan package:discover --ansi
 
-echo "==> Caching config..."
-php artisan config:cache
+# echo "==> Caching config..."
+# php artisan config:cache
 
-echo "==> Caching routes..."
-php artisan route:cache
+# echo "==> Caching routes..."
+# php artisan route:cache
 
-echo "==> Caching views..."
-php artisan view:cache
+# echo "==> Caching views..."
+# php artisan view:cache
 
 php artisan queue:clear redis
 php artisan queue:clear redis --queue=emails

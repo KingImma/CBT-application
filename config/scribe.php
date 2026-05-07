012cf7b6f22cb9cdd2718ad589ee2d7679354374
KingImma <07066611a@gmail.com>
2026-05-07 11:48:29 +0100

    fix: remove scribe enum dependency to prevent deployment crash

--- config/scribe.php
+++ config/scribe.php
@@ -116,7 +116,7 @@ return [
         'default' => true,
 
         // Where is the auth value meant to be sent in a request?
-        'in' => AuthIn::BEARER->value,
+        'in' => 'bearer',
 
         // The name of the auth parameter (e.g. token, key, apiKey) or header (e.g. Authorization, Api-Key).
         'name' => 'key',

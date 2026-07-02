e9216a1d5cc8c5ec07c535e0a665abbad59b6edd
KingImma <07066611a@gmail.com>
2026-07-02 16:30:29 +0100

    fix: update failed notification migration

--- database/migrations/tenant/2026_07_02_144342_fix_notifications_notifiable_id_column_type.php
+++ database/migrations/tenant/2026_07_02_144342_fix_notifications_notifiable_id_column_type.php
@@ -1,15 +1,22 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
 
 return new class extends Migration
 {
     public function up(): void
     {
         Schema::table('notifications', function (Blueprint $table) {
-            $table->uuid('notifiable_id')->change();
+            $table->dropIndex(['notifications_notifiable_type_notifiable_id_index']);
+        });
+
+        DB::statement('ALTER TABLE notifications ALTER COLUMN notifiable_id TYPE uuid USING notifiable_id::uuid;');
+
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->index(['notifiable_type', 'notifiable_id']);
         });
     }
 

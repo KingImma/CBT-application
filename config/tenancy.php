cb2015b1922d3570f38ef2c453bef8be50870ca1
KingImma <07066611a@gmail.com>
2026-09-02 12:40:21 +0100

    chore: update config

--- config/tenancy.php
+++ config/tenancy.php
@@ -62,7 +62,7 @@ return [
          * Tenant database names are created like this:
          * prefix + tenant_id + suffix.
          */
-        'prefix' => 'tenant_',
+        'prefix' => '',
         'suffix' => '',
 
         /**
